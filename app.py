e1741d115721c4d56750add8fcf2b0c15bb56ef3
Alex Gurvets <agurvets@gmail.com>
2023-08-29 18:56:11 -0700

    add streamlit app

---
+++ app.py
@@ -0,0 +1,126 @@
+import openai
+import streamlit as st
+from streamlit_chat import message
+
+# Setting page title and header
+st.set_page_config(page_title="AskMyTeam", page_icon=":robot_face:")
+
+# Set org ID and API key
+openai.organization = st.secrets['openai_organization']
+openai.api_key = st.secrets['openai_api_key']
+
+advisors = st.multiselect(
+    'Recruit your team',
+    ['Software Engineer', 'Doctor', 'Entrepreneur', 'Life Coach', 'Career Coach', 'Financial Advisor', 'Lawyer', 'Academic Advisor',
+     'Artist', 'Environmental Advisor', 'Fitness Coach', 'Historian', 'Investor'],
+    ['Life Coach', 'Financial Advisor', 'Software Engineer'])
+
+update_team_button = st.button("Save", key="update")
+
+st.markdown("<h2 style='text-align: center;'>aight team, here's the deal...</h1>", unsafe_allow_html=True)
+
+# Initialise session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You're my board of advisors, consisting of: " + ', '.join(advisors) + ". When I say something, each advisor should respond with some actionable advice based on their particular background."}
+    ]
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = []
+if 'cost' not in st.session_state:
+    st.session_state['cost'] = []
+if 'total_tokens' not in st.session_state:
+    st.session_state['total_tokens'] = []
+if 'total_cost' not in st.session_state:
+    st.session_state['total_cost'] = 0.0
+
+# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+st.sidebar.title("Settings")
+model_name = st.sidebar.radio("Choose a model:", ("GPT-4", "GPT-3.5"))
+counter_placeholder = st.sidebar.empty()
+counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+# Map model names to OpenAI model IDs
+if model_name == "GPT-3.5":
+    model = "gpt-3.5-turbo"
+else:
+    model = "gpt-4"
+
+def clear():
+    st.session_state['generated'] = []
+    st.session_state['past'] = []
+    st.session_state['messages'] = [
+        {"role": "system", "content": "You're my board of advisors, consisting of: " + ', '.join(advisors) + ". When I say something, each advisor should respond with some actionable advice based on their particular background."}
+    ]
+    st.session_state['number_tokens'] = []
+    st.session_state['model_name'] = []
+    st.session_state['cost'] = []
+    st.session_state['total_cost'] = 0.0
+    st.session_state['total_tokens'] = []
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+# reset everything
+if clear_button:
+    clear()
+
+if update_team_button:
+    clear()
+
+# generate a response
+def generate_response(prompt, advisors):
+    st.session_state['messages'].append({"role": "user", "content": prompt})
+
+    completion = openai.ChatCompletion.create(
+        model=model,
+        messages=st.session_state['messages']
+    )
+    response = completion.choices[0].message.content
+    st.session_state['messages'].append({"role": "assistant", "content": response})
+
+    # print(st.session_state['messages'])
+    total_tokens = completion.usage.total_tokens
+    prompt_tokens = completion.usage.prompt_tokens
+    completion_tokens = completion.usage.completion_tokens
+    return response, total_tokens, prompt_tokens, completion_tokens
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input, advisors)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+        st.session_state['model_name'].append(model_name)
+        st.session_state['total_tokens'].append(total_tokens)
+
+        # from https://openai.com/pricing#language-models
+        if model_name == "GPT-3.5":
+            cost = total_tokens * 0.002 / 1000
+        else:
+            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+        st.session_state['cost'].append(cost)
+        st.session_state['total_cost'] += cost
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
+            st.write(
+                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+# TODO: do a separate call for each expert. parallelize them, collect all responses
